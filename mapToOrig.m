ace19bc0b90acbf72541c3cc3787864fb0856afd
writetolavanya <writetolavanyav@gmail.com>
2025-03-19 15:34:02 -0500

    MapToOrig recovery fucntion updated to include new face's indexing

--- mapToOrig.m
+++ mapToOrig.m
@@ -1,7 +1,10 @@
 
 function mapToOrig()
-    filename1 = '/Users/lavanyavaddavalli/Desktop/lppd/data/S58.2022/S58_bb7_b';
-    filename2 = '/Users/lavanyavaddavalli/Desktop/lppd/data/S58.2022/S58_sub';
+    % filename1 = '/Users/lavanyavaddavalli/Desktop/lppd/tools_latest/data/KF_WM_TV_straight_LCC';
+    % filename2 = '/Users/lavanyavaddavalli/Desktop/lppd/tools_latest/data/KF_WM_TV_CoW3_fix_obvious';
+
+    filename1 = '/Users/lavanyavaddavalli/Desktop/lppd/data/S58 2022/S58_bb7';
+    filename2 = '/Users/lavanyavaddavalli/Desktop/lppd/tools_latest/data/S58_subnwk';
     
     nwk = nwkHelp.load(filename1);
     subnwk = nwkHelp.load(filename2);
@@ -22,15 +25,8 @@ function findOrigIdx(nwk, subnwk, filename2)
     sortedTmp = sort(tmpFaces, 2);
     sortedNwk = sort(nwk.faceMx(:,2:3), 2);
     [~, origFIdx] = ismember(sortedTmp, sortedNwk, 'rows');
-    
-    % Stores the original diameter for each face in sub network
-    origDia = nwk.dia(origFIdx);
 
-    % save orginal PIdx file, FIdx, diameter file
-    fileID = fopen([filename2, '.dia'], 'w');
-    fprintf(fileID, '%f\n', origDia);
-    fclose(fileID);
-
+    % save orginal PIdx file, FIdx file
     fileID = fopen([filename2, '.originalPIdx'], 'w');
     fprintf(fileID, '%d\n', origPIdx);
     fclose(fileID);
@@ -38,6 +34,22 @@ function findOrigIdx(nwk, subnwk, filename2)
     fileID = fopen([filename2, '.originalFIdx'], 'w');
     fprintf(fileID, '%d\n', origFIdx);
     fclose(fileID);
+    
+    % Stores the original diameter for each face in sub network
+    % For new faces not in reference mesh, diameter will 
+    % be mean of left and right max
+    origDia = ones(size(origFIdx)); 
+    validFaces = origFIdx > 0;
+    origDia(validFaces) = nwk.dia(origFIdx(validFaces));
+
+    newFaces = ~validFaces;
+    origDia(newFaces) = arrayfun(@(p1, p2) avgDia(nwk, p1, p2), inletPIdx(newFaces), outletPIdx(newFaces));
+
+    % Save original diameter file
+    fileID = fopen([filename2, '.dia'], 'w');
+    fprintf(fileID, '%f\n', origDia);
+    fclose(fileID);
+
 end
 
 function [ptIdx] = findNearestPoint(ptCoords, nwk)
@@ -47,3 +59,22 @@ function [ptIdx] = findNearestPoint(ptCoords, nwk)
     ptIdx = idx;
     ptIdx(minD >= tol) = 0;
 end
+
+function [meanDia] = avgDia(nwk, origP1, origP2)
+
+    meanDia = 1; %default
+
+    % cases where origP1, origP2 are 0, then one of the pts is not on mesh, exit
+    if (origP1 == 0 || origP2 == 0); return; end
+
+    % all faces that point p1 is connected to are taken
+    faceIdx1 = find(nwk.faceMx(:,2) == origP1 | nwk.faceMx(:,3) == origP1);
+
+    % all faces that point p2 is connected to are taken
+    faceIdx2 = find(nwk.faceMx(:,2) == origP2 | nwk.faceMx(:,3) == origP2);
+
+    leftMaxDia = max(nwk.dia(faceIdx1));
+    rightMaxDia = max(nwk.dia(faceIdx2));
+    
+    meanDia = mean([leftMaxDia, rightMaxDia]);
+end    
